03b8cffddced87e4f5169d3313d270e865476075
longpc <conglongfcb93@gmail.com>
2026-03-16 14:44:03 +0700

    add github actions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,23 +5,26 @@ npm ci
 
 echo "Building the package..."
 npm run build:publish
+
 cd dist
 
-PACKAGE_NAME=$(node -p "require('./package.json').name.split('/')[1]")
+PACKAGE_NAME=$(node -p "require('./package.json').name")
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
-NPM_PACKAGE_SCOPE=$(node -p "require('./package.json').name.split('/')[0]")
-NPM_REGISTRY="https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/packages/npm/"
 
-echo "Setting up .npmrc for GitLab registry..."
-echo "${NPM_PACKAGE_SCOPE}:registry=${NPM_REGISTRY}" > .npmrc
-echo "//${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/packages/npm/:_authToken=${CI_JOB_TOKEN}" >> .npmrc
+echo "Setting up .npmrc for GitHub Packages..."
+
+echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" > .npmrc
+echo "@${GITHUB_REPOSITORY_OWNER}:registry=https://npm.pkg.github.com" >> .npmrc
 
 echo "Checking if ${PACKAGE_NAME}@${PACKAGE_VERSION} is already published..."
-if npm view "${PACKAGE_NAME}"@"${PACKAGE_VERSION}" --registry "${NPM_REGISTRY}" > /dev/null 2>&1; then
+
+if npm view "${PACKAGE_NAME}"@"${PACKAGE_VERSION}" --registry https://npm.pkg.github.com > /dev/null 2>&1; then
   echo "Version ${PACKAGE_VERSION} already published. Skipping publish."
   exit 0
 fi
 
 echo "Publishing ${PACKAGE_NAME}@${PACKAGE_VERSION}..."
-npm publish --registry "${NPM_REGISTRY}"
+
+npm publish --registry https://npm.pkg.github.com
+
 echo "✅ Published successfully."
